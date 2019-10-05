ae03fbd4c3a8a310bf82cbbecc8ed75aeb251e35
jmackert <jonahbmackert@gmail.com>
2019-10-05 10:30:21 -0400

    fixed typo

--- src/main/java/reviewsite/Category.java
+++ src/main/java/reviewsite/Category.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+
 
 @Entity
 public class Category {
